65d95d03e438f3dd7af0acd10deae6de77f8fd43
Ricardo Rincón <ricardorincon911@gmail.com>
2020-02-20 18:35:48 +0000

    Added create action to save to db (controller) and corrected class name in contact.rb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new #Gets filled out by the form_for code in new.html.erb
   end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "Message sent." #Redirected to a blank form
+    else 
+      redirect_to new_contact_path, notice: "An error occurred."
+    end
+  end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end 
 end
