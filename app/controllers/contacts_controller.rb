98446270f1821bf63e96ae750f63d84d75fb46d0
Ricardo Rincón <ricardorincon911@gmail.com>
2020-02-22 10:24:37 +0000

    Added form validations to model file

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,9 @@
 class ContactsController < ApplicationController
-  def new
+  def new #This gets invoked whenever a user arrives at the form page
     @contact = Contact.new #Gets filled out by the form_for code in new.html.erb
   end
   
-  def create
+  def create #Code run when the user clicks 'Submit'
     @contact = Contact.new(contact_params)
     if @contact.save
       redirect_to new_contact_path, notice: "Message sent." #Redirected to a blank form
@@ -12,8 +12,8 @@ class ContactsController < ApplicationController
     end
   end
   
-  private
+  private #This is a security feature that whitelists and mass-assigns attributes
     def contact_params
-      params.require(:contact).permit(:name, :email, :comments)
+      params.require(:contact).permit(:name, :email, :comments) 
     end 
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact < ActiveRecord::Base
-    
+  #Unless these three fields return true by having been filled out, the
+  #form will not be saved
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
