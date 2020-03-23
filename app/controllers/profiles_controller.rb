0368f31708124c01c1d5dcb560da95832f3b2516
Ricardo Rincón <ricardorincon911@gmail.com>
2020-03-23 19:13:12 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,6 @@ class ProfilesController  < ApplicationController
   # When user makes a GET request to /users/:user_id/profile/new
   def new
     # Render a blank profile details form
+  @profile = Profile.new
   end
 end
